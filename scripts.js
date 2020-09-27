62950258679f94368087012f5c802fa59a4d3c4b
Nik Rowe <NikRowe7@Gmail.com>
2020-09-27 11:55:44 -0700

    Volume/Speed Sliders

--- scripts.js
+++ scripts.js
@@ -27,10 +27,26 @@ function skip() {
     video.currentTime += parseFloat(this.dataset.skip)
 }
 
+function sliders() {
+    /* Wes used this 1 liner however I came oup with the below ugly code on my own so I wanted to keep it in here for lead devs actually looking, to see my raw work */
+    // video[this.name] = this.value  //
+    if (this.name === 'playbackRate') {
+        video.playbackRate = this.value
+    } else {
+        video.volume = this.value
+    }
+}
+
+function handleProgress() {
+    
+}
+
 // hook up event listeners //
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 
-toggle.addEventListener('click', togglePlay) 
+toggle.addEventListener('click', togglePlay)
 skipButtons.forEach(button => button.addEventListener('click', skip))
+ranges.forEach(range => range.addEventListener('change', sliders))
+ranges.forEach(range => range.addEventListener('mousemove', sliders))
